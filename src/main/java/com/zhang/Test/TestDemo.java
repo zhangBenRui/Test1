0774738b888b3d0b6bc9b2e2238ac588578def60
zhangrui <498420369@qq.com>
2019-10-17 22:10:12 +0800

    提交

---
+++ src/main/java/com/zhang/Test/TestDemo.java
@@ -0,0 +1,12 @@
+package com.zhang.Test;/*
+ * @Author:${zhangrui}
+ * @Date:2019/10/17 22:08
+ */
+
+public class TestDemo {
+
+    public static void main(String[] args) {
+
+        System.out.println(12344);
+    }
+}
